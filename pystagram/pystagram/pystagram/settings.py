51221d635a256869298c1bdffdd7b2228adf0f51
gooiljung <junggooil9191@gmail.com>
2018-02-13 14:52:36 +0900

    pystagram

--- pystagram/pystagram/pystagram/settings.py
+++ pystagram/pystagram/pystagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*-jev+^v_fw(&pzo0-re4l+)@-0fzgrnp%bl2a#a29i+im5i90'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
